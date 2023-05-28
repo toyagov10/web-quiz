353c7c0ac96f4538b9670f40f1912024c299298e
Anatolii Iagov <iagov@devexperts.com>
2023-05-28 10:51:37 +0100

    Initial Commit

---
+++ src/main/java/com/quiz/WebQuizApplication.java
@@ -0,0 +1,12 @@
+package com.quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebQuizApplication.class, args);
+	}
+}
